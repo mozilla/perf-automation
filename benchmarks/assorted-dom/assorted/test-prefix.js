746183dc3e3fd85313d98acdc1efa02115909f2f
Robert Wood <rwood@Roberts-MacBook-Pro-1927.local>
2018-08-09 12:44:33 -0400

    Add assorted-dom benchmark from AWFY

---
+++ benchmarks/assorted-dom/assorted/test-prefix.js
@@ -0,0 +1,2 @@
+var tests = [ "misc-test", "misc-worker-getname-performance-getter", "misc-worker-getprop-performance-getter", "misc-window-getname-performance-getter", "misc-window-getprop-performance-getter" ];
+var categories = [ "misc" ];
